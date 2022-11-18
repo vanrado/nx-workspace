b537fccca19acb9cb1b553656dd1c2d14152b3fd
Radovan Racak <radovan.racak@gmail.com>
2022-11-18 14:27:01 +0100

    feat(interval-timer): tailwind

--- playground/apps/interval-timer/src/app/app.tsx
+++ playground/apps/interval-timer/src/app/app.tsx
@@ -1,11 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
     <>
-      <NxWelcome title="interval-timer" />
       <div />
     </>
   );
